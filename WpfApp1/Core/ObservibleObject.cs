448df3c4571ac9651b8a80e3506688dc6bce80b3
Юрий <@DESKTOP-NL8SGRD>
2022-11-17 22:56:42 +0700

    Добавьте файлы проекта.

---
+++ WpfApp1/Core/ObservibleObject.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp1.Core
+{
+    internal class ObservibleObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? name = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(name);
+            return true;
+        }
+    }
+}
---
+++ WpfApp1/MVVM/ViewModel/MainWindowModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using WpfApp1.Core;
+using WpfApp1.MVVM.View;
+
+namespace WpfApp1.MVVM.ViewModel
+{
+    internal class MainWindowModel : ObservibleObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public HomeViewModel HomeVm { get; set; }
+        public DiscoveryView DiscoveryVm { get; set; }
+
+
+        private object? _currentView;
+
+        public object? CurrentView
+        {
+            get => _currentView;
+            set => Set(ref _currentView, value);
+        }
+
+
+        public MainWindowModel()
+        {
+            HomeVm = new HomeViewModel();
+            DiscoveryVm = new DiscoveryView();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o => { CurrentView = HomeVm; });
+            DiscoveryViewCommand = new RelayCommand(o => { CurrentView = DiscoveryVm; });
+        }
+    }
+}
